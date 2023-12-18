aa0c2a9b17653c0abbdebbcbaa12a070c2868bc9
Kirill Vnukov <vnukov.k@introvert.bz>
2023-12-18 22:37:38 +0300

    Кроп объекта

--- ImageAnalysis.py
+++ ImageAnalysis.py
@@ -15,5 +15,27 @@ class ImageAnalysis:
         return np.array([image])  # TODO: Стоит заглушка, нужно реализовать.
 
     def compute_perceptual_hashes(self, objects, hashes):
-        # TODO: Нужно реализовать.
+        for i in range(objects.shape[0]):
+            min_y = objects.shape[1]
+            min_x = objects.shape[2]
+            max_y = 0
+            max_x = 0
+
+            for y in range(objects.shape[1]):
+                for x in range(objects.shape[2]):
+                    min_x = min(x, min_x) if objects[i][y][x] else min_x
+                    min_y = min(y, min_y) if objects[i][y][x] else min_y
+                    max_x = max(x, max_x) if objects[i][y][x] else max_x
+                    max_y = max(y, max_y) if objects[i][y][x] else max_y
+
+            height = max_y - min_y + 1
+            width = max_x - min_x + 1
+
+            max_axis = max(height, width)
+
+            cropped_object = np.zeros((max_axis, max_axis))
+            for y in range(height):
+                for x in range(width):
+                    cropped_object[y][x] = objects[i][min_y + y][min_x + x]
+
         return hashes
